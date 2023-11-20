b61a14ad6ca72e7e0a33e457002aacf7c2df8109
Daniel <dannyking07@outlook.es>
2023-11-20 01:49:13 -0500

    feat: [config routing]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { PageNotFoundComponent } from './error/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path: 'About', component: AboutComponent},
+  {path: 'Contacts', component: ContactsComponent},
+  {path: 'Projects', component: ProjectsComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
