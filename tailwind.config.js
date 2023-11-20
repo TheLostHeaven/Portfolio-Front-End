691b55b804ed7115296976d7df74b02a6768c819
Daniel <dannyking07@outlook.es>
2023-11-19 19:26:03 -0500

    feat: [config tailwind]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+// module.exports = {
+//   darkMode: 'class',
+//   content: {
+//     enabled: true,
+//     content: [
+//       './src/**/*.{html,ts}',
+//     ],
+//   },
+// }
+
+
 module.exports = {
+  darkMode: 'class',
   content: {
     enabled: true,
     content: [
       './src/**/*.{html,ts}',
     ],
   },
+  theme: {
+    extend: {
+      colors: {
+        customPrimaryColor: '#C778DD',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
 }
