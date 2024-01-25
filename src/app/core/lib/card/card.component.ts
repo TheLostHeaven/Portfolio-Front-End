4d6be3f4f6026e2198e88a1f2c93aa9f2586b912
Daniel <Dannyking07@outlook.es>
2024-01-25 17:04:02 -0500

    feat lib -> card component

--- src/app/core/lib/card/card.component.ts
+++ src/app/core/lib/card/card.component.ts
@@ -1,12 +1,22 @@
 import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Publication,  } from 'src/app/models/item';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.sass']
 })
+
 export class CardComponent {
+  @Input() publication!: Publication;
+  imgSrc: SafeUrl = this.sanitizer.bypassSecurityTrustUrl('');
 
+  constructor(private sanitizer: DomSanitizer) {}
 
+  ngOnChanges(): void {
+    let objectURL = this.publication.img
+    this.imgSrc = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+  }
 }
 
