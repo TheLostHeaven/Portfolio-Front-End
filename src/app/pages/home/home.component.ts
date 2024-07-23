3c7a414349d05b8c114a422b308b6cc6ec0e3571
Daniel Molina <dannyking07@outlook.es>
2024-07-23 05:15:36 -0500

    update(card -> home): modifique card and home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,45 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit} from '@angular/core';
+import { Publication } from 'src/app/models/item';
 import { HomeService } from 'src/app/core/service/home/home.service';
-import { Publication, } from 'src/app/models/item';
-import { Observable, forkJoin, map, switchMap } from 'rxjs';
-
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit{
-  publication$!: Observable<Publication[]>;
-  // img$!: Observable<img[]>;
-  loading: boolean = true;
-  // //imgView: [];
-  //img$!: [];
 
 
-  constructor(private http: HttpClient, private HomeService: HomeService) { }
+export class HomeComponent implements OnInit {
 
-    ngOnInit(): void {
-    this.getDatos();
-  }
+  posts: Publication[] = []
 
-  getDatos(): void {
-    this.HomeService.getPubli().pipe(
-    switchMap((res1: Publication[]) => {
-      const Observables = res1.map (pub =>
-        this.HomeService.getImg(pub.img).pipe(
-          map(res2 => ({...pub, img: res2}))
-        )
-      );
+  constructor (private service: HomeService) {}
 
-      return forkJoin(Observables)
-    })
-    ).subscribe(res => {
-      console.log(res),
-      () => this.loading = false
-    });
+  ngOnInit(): void {
+    this.service.getPubli().subscribe(
+      data => {this.posts = data},
+      error => {console.error('error', error)}
+    );
   }
 }
 
